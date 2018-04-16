664cc1a2ddaf26f03c695be05ffe4e4d51fd45c0
martyhandicap11 <mcooney17@gmail.com>
2018-04-16 14:20:00 +0100

    Fixed jar file errors and jpaMappingContext

--- src/main/java/com/martycooney/reservation/ReservationApplication.java
+++ src/main/java/com/martycooney/reservation/ReservationApplication.java
@@ -1,12 +1,26 @@
 package com.martycooney.reservation;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.jms.JndiConnectionFactoryAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@EnableAutoConfiguration(exclude =
+        {JndiConnectionFactoryAutoConfiguration.class,DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class,JpaRepositoriesAutoConfiguration.class,DataSourceTransactionManagerAutoConfiguration.class})
 
 @SpringBootApplication
-public class ReservationApplication {
+public class ReservationApplication
+{
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReservationApplication.class, args);
+	public static void main(String[] args)
+    {
+		SpringApplication.run
+                (ReservationApplication.class, args);
 	}
+
 }
